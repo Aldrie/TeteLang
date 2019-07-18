11f0c6f4c5855a1d9da9832d352531e599898964
aldriê mizobuti <aldriejapa@outlook.com>
2019-07-18 03:25:54 -0300

    Upgrade

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname + '/pages/index.html'));
 });
 
+app.get("/help", (req, res)=>{
+    res.sendFile(path.join(__dirname + '/pages/help.html'));
+});
+
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname + '/pages/error.html'));
+});
+
 app.listen(port, ()=>{
     console.log(`Online on port [${port}]`);
 });
